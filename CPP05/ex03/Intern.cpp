02b72d00ea0b36508e735dfbdf2cf8278e032655
mahmmom <moh.aya@hotmail.com>
2024-07-14 16:11:24 +0400

    evaluated 05

--- CPP05/ex03/Intern.cpp
+++ CPP05/ex03/Intern.cpp
@@ -33,80 +33,6 @@ Intern::~Intern()
 	std::cout << "Intern destructor" << std::endl;
 }
 
-// int	levFinder(std::string level)
-// {
-// 	std::string levels[3] = {"shrubbery creation", "robotomy request", "presidential pardon"};
-// 	std::cout << level << std::endl;
-//     for (int i = 0; i < 3; ++i)
-// 	{
-//         if (levels[i] == level)
-//             return(i);
-//     }
-// 	return(3);
-// }
-
-// AForm *Intern::makeForm(std::string formName, std::string target)
-// {
-// 	std::string newName;
-// 	int	j = 0;
-// 	for (size_t i = 0; i < formName.size(); i++)
-// 	{
-// 		while(formName[j] == ' ')
-// 			j++;
-// 		std::cout << formName << std::endl;
-// 		newName[i] = (char)std::towlower(formName[i]);
-// 		// std::cout << newName[i] << std::endl;
-// 		if (i == formName.size()) {
-// 			newName[i] = 0;
-// 			break;
-// 		}
-// 	}
-// 	std::cout << newName << std::endl;
-// 	int formLevel = levFinder(newName);
-// 	switch (formLevel)
-// 	{
-// 	case 0:
-// 		return new ShrubberyCreationForm(target);
-// 		// break;
-// 	case 1:
-// 		return new RobotomyRequestForm(target);
-// 		// break;
-// 	case 2:
-// 		return new PresidentialPardonForm(target);
-// 		// break;
-// 	default:
-// 		std::cout << "Form name given doesn't exist" << std::endl;
-// 		return NULL;
-// 		// break;
-// 	}
-// 	return NULL;
-// }
-
-// AForm *Intern::makeForm(std::string formName, std::string target)
-// {
-//     // Initialize newName with the size of formName
-//     std::string newName(formName.size(), '\0');
-
-// 	for (size_t i = 0; i < formName.size(); i++)
-//     {
-//         newName[i] = (char)std::tolower(formName[i]); // Use std::tolower for char
-//     }
-//     std::cout << "Form name in lowercase: " << newName << std::endl;
-//     int formLevel = levFinder(newName);
-//     switch (formLevel)
-//     {
-// 		case 0:
-// 			return new ShrubberyCreationForm(target);
-// 		case 1:
-// 			return new RobotomyRequestForm(target);
-// 		case 2:
-// 			return new PresidentialPardonForm(target);
-// 		default:
-// 			std::cout << "Form name given doesn't exist" << std::endl;
-// 			return NULL;
-//     }
-// }
-
 AForm* Intern::ShrubberyCreation(std::string target)
 {
 	return new ShrubberyCreationForm(target);
@@ -135,8 +61,92 @@ AForm*	Intern::makeForm(std::string formName, std::string target)
     for (int i = 0; i < 3; ++i)
 	{
 		if (levels[i] == newName)
-            return (this->*formFunc[i])(target);
+        {
+			std::cout << "Creates form " << newName << std::endl;
+			return (this->*formFunc[i])(target);
+		}
     }
 	throw InvalidFormException();
 	return NULL;
 }
+
+/*
+		Different Approach
+
+
+int	levFinder(std::string level)
+{
+	std::string levels[3] = {"shrubbery creation", "robotomy request", "presidential pardon"};
+	std::cout << level << std::endl;
+    for (int i = 0; i < 3; ++i)
+	{
+        if (levels[i] == level)
+            return(i);
+    }
+	return(3);
+}
+
+AForm *Intern::makeForm(std::string formName, std::string target)
+{
+	std::string newName;
+	int	j = 0;
+	for (size_t i = 0; i < formName.size(); i++)
+	{
+		while(formName[j] == ' ')
+			j++;
+		std::cout << formName << std::endl;
+		newName[i] = (char)std::towlower(formName[i]);
+		// std::cout << newName[i] << std::endl;
+		if (i == formName.size()) {
+			newName[i] = 0;
+			break;
+		}
+	}
+	std::cout << newName << std::endl;
+	int formLevel = levFinder(newName);
+	switch (formLevel)
+	{
+	case 0:
+		return new ShrubberyCreationForm(target);
+		// break;
+	case 1:
+		return new RobotomyRequestForm(target);
+		// break;
+	case 2:
+		return new PresidentialPardonForm(target);
+		// break;
+	default:
+		std::cout << "Form name given doesn't exist" << std::endl;
+		return NULL;
+		// break;
+	}
+	return NULL;
+}
+
+AForm *Intern::makeForm(std::string formName, std::string target)
+{
+    // Initialize newName with the size of formName
+    std::string newName(formName.size(), '\0');
+
+	for (size_t i = 0; i < formName.size(); i++)
+    {
+        newName[i] = (char)std::tolower(formName[i]); // Use std::tolower for char
+    }
+    std::cout << "Form name in lowercase: " << newName << std::endl;
+    int formLevel = levFinder(newName);
+    switch (formLevel)
+    {
+		case 0:
+			return new ShrubberyCreationForm(target);
+		case 1:
+			return new RobotomyRequestForm(target);
+		case 2:
+			return new PresidentialPardonForm(target);
+		default:
+			std::cout << "Form name given doesn't exist" << std::endl;
+			return NULL;
+    }
+}
+
+
+*/
