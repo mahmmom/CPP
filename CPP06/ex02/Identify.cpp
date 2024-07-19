355ceddf5dc8c163e915372b168bcc608e159eb1
mahmmom <moh.aya@hotmail.com>
2024-07-19 20:52:19 +0400

    cpp06/ex02/not complete

---
+++ CPP06/ex02/Identify.cpp
@@ -0,0 +1,17 @@
+#include "Identify.hpp"
+
+Base *generate(void)
+{
+	
+	return nullptr;
+}
+
+void identify(Base *p)
+{
+
+}
+
+void identify(Base &p)
+{
+
+}
--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -1,7 +1,11 @@
-
+#include "Identify.hpp"
 
 
 int main()
 {
-
+	Base *base;
+	base = generate();
+	identify(base);
+	identify(*base);
+	delete base;
 }
